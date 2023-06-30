990610d92811a13e39bec818099dcc34424f81a7
Alexander Arcari <alexanderarcarim@gmail.com>
2023-06-30 15:12:36 -0400

    Added Welcome message and Menu options

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -3,12 +3,133 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include <fstream>
+
+using namespace std;
+
+void welcome()
+{
+    
+    int rowOfPeriods2 = 45;
+    string welcome = "Classroom Grades Program";
+    int rowOfPeriods1 = 90 + welcome.length();
+
+    cout << setfill('-');
+    cout << setw(rowOfPeriods1) << "" << endl;
+    cout << setw(rowOfPeriods2 +welcome.length()) << welcome << setw(rowOfPeriods2) << "" << endl;
+    cout << setw(rowOfPeriods1) << "" << endl << endl;
+
+}
+void menu() 
+{
+    char commandEnterGrades = 'e';
+    char commandCalculateAverage = 'c';
+    char commandGenerateReport = 'r';
+    char commandSaveToFile = 's';
+    char commandMenu = 'm';
+    char commandExit = 'x';
+
+    cout << endl << endl;
+
+    cout << "Enter '" << commandEnterGrades << "' to enter grades." << endl;
+    cout << "Enter '" << commandCalculateAverage << "' to calculate averages." << endl;
+    cout << "Enter '" << commandGenerateReport << "' to generate a grade report for the class." << endl;
+    cout << "Enter '" << commandSaveToFile << "' to save work to file." << endl;
+    cout << "Enter '" << commandMenu << "' to call the menu options." << endl;
+    cout << "Enter '" << commandExit << "' to exit the program." << endl << endl << endl;
+
+}
+
+void enterGrades() 
+{
+
+}
+
+
+void calculateAverages() 
+{ //this can also be an int
+
+}
+
+bool readfromFile() 
+{
+
+    return true;
+}
+
+void generateReport()
+{
+
+}
+
+void saveToFile()
+{
+
+}
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Variable Declaration 
+    char commandEnterGrades = 'e';
+    char commandCalculateAverage = 'c';
+    char commandGenerateReport = 'r';
+    char commandSaveToFile = 's';
+    char commandMenu = 'm';
+    char commandExit = 'x';
+    char userOption;
+
+
+    // Call Functions Welcome and Menu
+    welcome();
+    menu();
+
+
+    userOption = 't';
+
+    while (userOption != commandExit)
+    {
+
+        cout << "Please enter an operation to perform:";
+        cin >> userOption;
+
+        if (customerOption == commandGetCount) //call GetCountofTeam function
+        {
+            //GetCountOfTeam();
+        }
+        else if (customerOption == commandGetMostWinning) //call GetMostWinningTeam function
+        {
+            //GetMostWinningTeam();
+        }
+        else if (customerOption == commandGetTeamDate) // call GetTeamForDate function 
+        {
+            //GetTeamForDate();
+        }
+        else if (customerOption == commandConvertCase) // call ConvertTeamNameCase function
+        {
+            cout << "Please enter the case to change ('u' = upper and 'l' = lower):";
+            cin >> customerOption;
+
+            if (customerOption == upperChoice) { //changes the value of to_upper depending on the customers option
+                to_upper = true;
+            }
+            else if (customerOption == lowerChoice)
+            {
+                to_upper = false;
+            }
+
+            ConvertTeamNameCase(to_upper, teamNames, SIZE_DATA);
+        }
+
+        else if (customerOption == commandExit) {
+            cout << "Good Bye." << endl;
+        }
+    }
+
+    // Return 0 to indicate success.
+    return 0;
+
 }
 
 
