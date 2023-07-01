5b3f56d4cbb393e3663240bdf1861255272e17c6
Alexander Arcari <alexanderarcarim@gmail.com>
2023-07-01 00:22:30 -0400

    Debugged the enter grades function, the calculate averages function is almost complete, and the generate report is almost complete.

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 const int MAX_STUDENTS = 10;
-const int MAX_GRADES = 5;
+const int MAX_GRADES = 2;
 
 void welcome()
 {
     int rowOfPeriods2 = 45;
-    string welcome = "Classroom Grades Program";
+    string welcome = " Classroom Grades Program ";
     int rowOfPeriods1 = 90 + welcome.length();
 
     cout << setfill('-');
@@ -39,16 +39,35 @@ void menu()
     cout << "Enter '" << commandExit << "' to exit the program." << endl << endl << endl;
 }
 
-void enterGrades(string names[], int grades[][MAX_GRADES], int numStudents, const int MAX_GRADES)
+void enterGrades(int& i, string names[], int grades[][MAX_GRADES], const int MAX_STUDENTS, const int MAX_GRADES)
 {
-    for (int i = 0; i < MAX_GRADES; i++) {
+    char userOption = 'y';
+    int examNumber = 1;
+
+    while (i < MAX_GRADES && userOption != 'n')
+    {
         cout << "Enter grades for Exam " << (i + 1) << endl;
-        for (int j = 0; j < numStudents; j++) {
-            cout << "Enter grades for " << names[j] << ":";
+        
+
+        for (int j = 0; j < MAX_STUDENTS; j++)
+        {
+            cout << "Enter grades for " << names[j] << ": ";
             cin >> grades[i][j];
+            cout << endl;
         }
+        i++;
+        if (i != MAX_GRADES) 
+        {
+            cout << "Would you like to enter the grades for exam " << (i) << "? ('y'= yes and 'n' = no): ";
+            cin >> userOption;
+        }
+        else if (i == MAX_GRADES) {
+            cout << "You've entered all of the exam grades for this semester. Time to calculate averages, final grades, and generate a report!" << endl;
+        }
+    
+
     }
-}
+} //
 
 bool readfromFile(ifstream& fileName, string names[])
 {
@@ -73,115 +92,102 @@ bool readfromFile(ifstream& fileName, string names[])
     }
 }
 
-void calculateAverages(int grades[][MAX_GRADES], int numStudents)
+void calculateAverages(int averageStudentScores[], string letterGrades[], string names[], int grades[][MAX_GRADES], const int MAX_STUDENTS, const int MAX_GRADES)
 {
-    int averageStudentScores[MAX_STUDENTS];
-    char letterGrades[MAX_STUDENTS];
 
-    for (int i = 0; i < numStudents; i++) {
-        int sum = 0;
+    for (int i = 0; i < MAX_STUDENTS; i++)
+    {
+        double sum = 0;
         for (int j = 0; j < MAX_GRADES; j++) {
-            sum += grades[i][j];
+            sum += grades[j][i];
         }
         averageStudentScores[i] = sum / MAX_GRADES;
 
         // Determine letter grade
-        if (averageStudentScores[i] >= 90) {
-            letterGrades[i] = 'A';
+        if (averageStudentScores[i] >= 95) {
+            letterGrades[i] = "A";
         }
-        else if (averageStudentScores[i] >= 80) {
-            letterGrades[i] = 'B';
+        else if (averageStudentScores[i] >= 90 && averageStudentScores[i] < 95) {
+            letterGrades[i] = "A-" ;
         }
-        else if (averageStudentScores[i] >= 70) {
-            letterGrades[i] = 'C';
+        else if (averageStudentScores[i] >= 85 && averageStudentScores[i] < 90) {
+            letterGrades[i] = "B";
         }
-        else if (averageStudentScores[i] >= 60) {
-            letterGrades[i] = 'D';
+        else if (averageStudentScores[i] >= 80 && averageStudentScores[i] < 85) {
+            letterGrades[i] = "B-";
+        }
+        else if (averageStudentScores[i] >= 75 && averageStudentScores[i] < 80) {
+            letterGrades[i] = "C";
+        }
+        else if (averageStudentScores[i] >= 70 && averageStudentScores[i] < 75) {
+            letterGrades[i] = "C-";
+        }
+        else if (averageStudentScores[i] >= 65 && averageStudentScores[i] < 70) {
+            letterGrades[i] = "D";
+        }
+        else if (averageStudentScores[i] >= 60 && averageStudentScores[i] < 65) {
+            letterGrades[i] = "D-";
         }
         else {
-            letterGrades[i] = 'F';
+            letterGrades[i] = "F";
         }
     }
 
     // Display the calculated averages and letter grades
-    cout << "Calculated averages and letter grades:" << endl;
-    for (int i = 0; i < numStudents; i++) {
-        cout << "Student " << (i + 1) << " - Average: " << averageStudentScores[i] << ", Grade: " << letterGrades[i] << endl;
+    cout << endl << "Calculated averages and letter grades:" << endl;
+    for (int i = 0; i < MAX_STUDENTS; i++) {
+        cout << names[i] << " - Average: " << averageStudentScores[i] << ", Grade: " << letterGrades[i] << endl;
     }
 }
 
-    //void calculateAverages(string names[], int scores[][MAX_GRADES], int SCORES, int averageScores[])
-    //{
-    //
-//void calculateAverages(string names[], int scores[][MAX_GRADES], int SCORES, int averageScores[])
-//{
-//
- //   int row;
-   // int col;
-    //int sum;
-
-    //for (row = 0; row < MAX_STUDENTS; row++)
-    //{
-     //   sum = 0;
-     //   for (col = 0; col < MAX_GRADES; col++)
-     //   {
-      //      sum = sum + scores[row][col];
-
-        //}
-        //averageScores[row] = sum / MAX_GRADES;
-
-    //}
-//}
-
-void readFromFile()
-{
-    ifstream myFile;
-    myFile.open("Student Names.txt");
-    if (myFile.is_open()) {
-        char mystring;
-        while (myFile) {
-            mystring = myFile.get();
-            cout << mystring;
-        }
-        myFile.close();
-    }
-    else {
-        cerr << "Error opening file." << endl;
-    }
-}
-
-void generateReport(string names[], int scores[][MAX_GRADES], int MAX_GRADES, int averageStudentScores[])
+void generateReport(int averageStudentScores[], string letterGrades[], string names[], int grades[][MAX_GRADES], const int MAX_GRADES, const int MAX_STUDENTS) //Ask for help
 {
     int periodsOne = 15;
     int periodsTwo = 10;
     int PeriodsThree = 8;
+    int averageColumn = 1;
 
     ofstream outFile;
 
     cout << setfill('.');
 
-    outFile.open("scores.dat");
+    outFile.open("Student Report.txt");
 
-    for (int i = 0; i < MAX_STUDENTS; i++) {
+    for (int i = 0; i < MAX_STUDENTS; i++)
+    {
         cout << left << setw(periodsOne) << names[i];
         outFile << left << setw(periodsOne) << names[i];
 
-        for (int j = 0; j < MAX_GRADES; j++) {
-            cout << right << setw(periodsTwo) << scores[i][j];
-            outFile << right << setw(periodsTwo) << scores[i][j];
+        for (int j = 0; j < MAX_GRADES; j++)
+        {
+            cout << right << setw(periodsTwo) << grades[j][i];
+            outFile << right << setw(periodsTwo) << grades[j][i];
 
         }
-        for (int x = 0; x < 1; x++) {
-            cout << right << setw(PeriodsThree) << averageStudentScores[i] << " ";
-            outFile << right << setw(PeriodsThree) << averageStudentScores[i] << " ";
+        for (int x = 0; x < averageColumn; x++) // Debug
+        {
+        
+            cout << right << setw(periodsTwo) << grades[i];
+            outFile << right << setw(periodsTwo) << grades[i];
+
         }
+        for (int y = 0; y < averageColumn; y++) // Debug
+        {
+
+            cout << right << setw(periodsTwo) << grades[i];
+            outFile << right << setw(periodsTwo) << grades[i];
+
+        }
+
         cout << endl;
         outFile << endl;
     }
+
+     
     outFile.close();
 }
 
-void saveToFile(string names[], int scores[][MAX_GRADES], int averageStudentScores[])
+void saveToFile(string names[], int grades[][MAX_GRADES])
 {
     ofstream myFile("Student Names.txt");
     if (myFile.is_open()) {
@@ -189,10 +195,10 @@ void saveToFile(string names[], int scores[][MAX_GRADES], int averageStudentScor
             myFile << "Student Name: " << names[i] << endl;
             myFile << "Grades: ";
             for (int j = 0; j < MAX_GRADES; j++) {
-                myFile << scores[i][j] << " ";
+                myFile << grades[i][j] << " ";
             }
             myFile << endl;
-            myFile << "Average Grade: " << averageStudentScores[i] << endl;
+            //myFile << "Average Grade: " << averageStudentScores[i] << endl;
             myFile << endl;
         }
         myFile.close();
@@ -212,12 +218,16 @@ int main()
     char commandMenu = 'm';
     char commandExit = 'x';
     char userOption;
+
+
     int grades[MAX_STUDENTS][MAX_GRADES];
     string names[MAX_STUDENTS];
+    int averageStudentScores[MAX_STUDENTS];
+    string letterGrades[MAX_STUDENTS];
 
+    int examNumber = 0;
     bool status;
 
-    string names[MAX_STUDENTS]; // array to store names
 
     ifstream myFile("Student Names.txt"); // open file 
 
@@ -225,9 +235,6 @@ int main()
     // Call Functions Welcome and Menu
     welcome();
     menu();
-
-
-
 
     status = readfromFile(myFile, names);
 
@@ -241,28 +248,16 @@ int main()
 
         if (userOption == commandEnterGrades) //call GetCountofTeam function
         {
-            cout << "Enter the number of students: ";
-            cin >> numStudents;
-
-            enterGrades(names, grades, numStudents, MAX_GRADES);
-
-            // Display the entered grades
-            cout << "Entered grades:" << endl;
-            for (int i = 0; i < numStudents; i++) {
-                cout << "Student " << (i + 1) << ": ";
-                for (int j = 0; j < MAX_GRADES; j++) {
-                    cout << grades[i][j] << " ";
-                }
-                cout << endl;
-            }
+             enterGrades(examNumber, names, grades, MAX_STUDENTS, MAX_GRADES);
+             
         }
         else if (userOption == commandCalculateAverage) //call GetMostWinningTeam function
         {
-            //GetMostWinningTeam();
+            calculateAverages(averageStudentScores, letterGrades, names, grades, MAX_STUDENTS, MAX_GRADES);
         }
         else if (userOption == commandGenerateReport) // call GetTeamForDate function 
         {
-            //GetTeamForDate();
+            generateReport(averageStudentScores, letterGrades, names, grades, MAX_GRADES, MAX_STUDENTS);
         }
         else if (userOption == commandSaveToFile) // call ConvertTeamNameCase function
         {
